e9fed700a2ed33f9419053263ca4cfa90e5127fe
Ryan24313 <79584392+Ryan24313@users.noreply.github.com>
2023-11-08 22:28:06 -0500

    added text

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from "./three.module.js"
 
-function loadTexture(texture) {
-	return new THREE.TextureLoader().load(texture)
+// set rows
+let main = document.getElementsByTagName('main')[0]
+for (let i = 0; i < main.children.length; i++) {
+	main.children[i].style.gridRow = i + 1
 }
 
 const scene = new THREE.Scene()
@@ -19,105 +21,84 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
-camera.position.y = 20
-camera.position.z = 100
 
-//LIGHTS
-const pointLight = new THREE.PointLight(0xFFFFFF, 1000, 1000)
-pointLight.position.set(0, 0, 50)
-
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1)
-
-scene.add(pointLight)
-scene.add(ambientLight)
-
-
-//HELPERS
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
-const axesHelper = new THREE.AxesHelper(50, 50, 50)
-// scene.add(lightHelper, gridHelper, axesHelper)
-
-
-const geoPog = new THREE.CylinderGeometry(41.37, 41.37, 6, 320)
-const texturePog = new THREE.TextureLoader().load('eevee.png')
-const matPog = new THREE.MeshStandardMaterial({
-	color: 0xFFFFFF,
-	wireframe: false,
-	map: texturePog
-})
-const pogMaterials = [
-	new THREE.MeshStandardMaterial({
-		color: 0xFFFFFF,
-		map: loadTexture('eevee_side.png'),
-		wireframe: false
-	}),
-	new THREE.MeshStandardMaterial({
-		color: 0xFFFFFF,
-		map: loadTexture('eevee.png'),
-		wireframe: false
-	}),
-	new THREE.MeshStandardMaterial({
-		color: 0xFFFFFF,
-		map: loadTexture('eevee_back.png'),
-		wireframe: false
-	})
-]
-
-
-const pog = new THREE.Mesh(geoPog, pogMaterials)
-pog.rotation.x = 45 * Math.PI / 180
-
-scene.add(pog)
-
-function newStar() {
-	const gemometry = new THREE.SphereGeometry(0.5, 24, 24);
-	const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
-	const star = new THREE.Mesh(gemometry, material);
-
-	const x = THREE.MathUtils.randFloatSpread(500)
-	const y = THREE.MathUtils.randFloatSpread(500) - 20
-	const z = THREE.MathUtils.randFloatSpread(500) - 100
-
-	star.position.set(x, y, z);
-	scene.add(star);
-}
-Array(1000).fill().forEach(newStar)
-
+// background
 const spaceTexture = new THREE.TextureLoader().load('space.jpg')
 scene.background = spaceTexture
 
-let t = document.body.getBoundingClientRect().top
+
+// lights
+const pointLight = new THREE.PointLight(0xFFFFFF, 600, 40)
+scene.add(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.2)
+scene.add(ambientLight)
+
+
+// helpers
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+const axesHelper = new THREE.AxesHelper(50, 50, 50)
+scene.add(lightHelper)
+// scene.add(gridHelper)
+// scene.add(axesHelper)
+
+let pictures = []
+
+class Picture {
+	constructor(
+		id,
+		size,
+		scale,
+		position,
+		src
+	) {
+		this.id = id
+		this.size = size
+		this.position = position
+		this.src = src
+		this.scale = scale
+
+		const geometry = new THREE.PlaneGeometry(this.size[0], this.size[1], 1, 1)
+		const texture = new THREE.TextureLoader().load(this.src)
+		const mesh = new THREE.MeshStandardMaterial({
+			map: texture
+		})
+		this.picture = new THREE.Mesh(geometry, mesh)
+
+		this.picture.position.set(...position)
+		this.picture.scale.set(this.scale, this.scale, this.scale)
+
+		scene.add(this.picture)
+	}
+}
+
+function addPicture(size, scale, position, src) {
+	pictures.push(new Picture(pictures.length, size, scale, position, src))
+}
+
+// pictures
+// addPicture([2200, 1700], 0.01, [0, -10, -20], './Images/ITF Certifacate.png')
+// addPicture([2200, 1700], 0.01, [0, -50, -20], './Images/OSHA Certifacate.png')
+
 
 function moveCamera() {
-	t = document.body.getBoundingClientRect().top
+	const scrollAmount = document.body.getBoundingClientRect().top
 
-	pog.rotation.x += 0.05
-	pog.rotation.y += 0.075
-	pog.rotation.z += 0.05
+	const cameraPosition = scrollAmount * 0.04
 
-	camera.position.x = t * -0.0002
-	camera.position.y = t * -0.0002
-	camera.position.z = t * -0.02 + 100
+
+	// pointLight.position.z = -20
+	pointLight.position.y = cameraPosition + 10
+	camera.position.y = cameraPosition
 }
+moveCamera()
 document.body.onscroll = moveCamera
 
 let maxScroll = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
 
 function animate(time) {
 	requestAnimationFrame(animate)
-
-	if (-1 * t >= maxScroll * 0.8) {
-		pog.rotation.x += 0.05 * 0.1
-		pog.rotation.y += 0.075 * 0.1
-		pog.rotation.z += 0.05 * 0.1
-
-		pog.position.x = Math.sin(pog.rotation.x) * camera.position.z * 0.7
-		pog.position.y = Math.sin(pog.rotation.y) * camera.position.z * 0.7
-		pog.position.z = Math.sin(pog.rotation.z) * camera.position.z * 0.07
-	} else {
-		pog.position.set(0, 0, 0)
-	}
 
 	renderer.render(scene, camera)
 }
