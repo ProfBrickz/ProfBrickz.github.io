73f13e1ec55946ed265fe29535d32dfb1976bce7
Ryan24313 <79584392+Ryan24313@users.noreply.github.com>
2023-12-13 21:35:50 -0500

    made pictures lock to elements

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "three"
 
 function degToRad(deg) {
 	return deg * (Math.PI / 180)
@@ -29,7 +29,7 @@ scene.background = spaceTexture
 
 
 // lights
-const pointLight = new THREE.PointLight(0xFFFFFF, 600, 40)
+const pointLight = new THREE.PointLight(0xFFFFFF, 400, 40)
 scene.add(pointLight)
 
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.4)
@@ -47,23 +47,40 @@ const axesHelper = new THREE.AxesHelper(50, 50, 50)
 let pictures = []
 
 class Picture {
-	constructor(
+	constructor({
 		id,
 		size,
 		scale,
-		position,
-		rotation,
+		linkedTo,
+		alignment = 'center',
+		offset = {},
+		depth = 10,
+		rotation = {},
 		src,
 		loop = false
-	) {
+	}) {
 		this.id = id
 		this.size = size
 		this.scale = scale
-		this.position = position
+		this.linkedTo = linkedTo
+		this.alignment = alignment
+		this.offset = offset
+		this.depth = depth
 		this.rotation = rotation
 		this.src = src
 		this.video
 		this.loop = loop
+
+		if (!offset.x) offset.x = 0
+		if (!offset.y) offset.y = 0
+
+		if (!rotation.x) rotation.x = 0
+		if (!rotation.y) rotation.y = 0
+		if (!rotation.z) rotation.z = 0
+
+		rotation.x = degToRad(rotation.x)
+		rotation.y = degToRad(rotation.y)
+		rotation.z = degToRad(rotation.z)
 
 		const geometry = new THREE.PlaneGeometry(this.size.x, this.size.y, 1, 1)
 		let texture
@@ -92,6 +109,8 @@ class Picture {
 		this.picture.scale.set(this.scale, this.scale, this.scale)
 
 		scene.add(this.picture)
+
+		pictures.push(this)
 	}
 
 	setSrc = (src) => {
@@ -124,215 +143,250 @@ class Picture {
 			this.scale * scale * 0.6,
 			this.scale * scale * 0.6
 		)
+	}
 
-		if (window.innerWidth <= 600) {
-			this.picture.position.set(
-				this.position.mobile.x * scale * 0.6,
-				this.position.mobile.y * scale,
-				this.position.mobile.z
-			)
-		} else {
-			this.picture.position.set(
-				this.position.desktop.x + this.position.desktop.x / scale,
-				this.position.desktop.y + this.position.desktop.y * -scale * 0.2,
-				this.position.desktop.z
-			)
+	setPosition = () => {
+		let element = document.getElementById(this.linkedTo)
+		let rect = element.getBoundingClientRect()
+
+		let domX = rect.x
+		let domY = rect.y
+
+		if (this.alignment.includes('right')) {
+			domX += rect.width
+		} else if (!this.alignment.includes('left')) {
+			domX += rect.width / 2
 		}
+
+		if (this.alignment.includes('bottom')) {
+			domY += rect.height
+		} else if (!this.alignment.includes('top')) {
+			domY += rect.height / 2
+		}
+
+		let newPosition = new THREE.Vector3()
+
+		newPosition.set(
+			((domX + this.offset.x) / window.innerWidth) * 2 - 1,
+			-((domY - this.offset.y) / window.innerHeight) * 2 + 1,
+			-this.depth
+		)
+
+		newPosition.unproject(camera)
+
+		newPosition.sub(camera.position).normalize()
+
+		let distance = -newPosition.z * 15
+
+		newPosition.x = camera.position.x + newPosition.x * distance
+		newPosition.y = camera.position.y + newPosition.y * distance
+		newPosition.z = camera.position.z + newPosition.z * distance
+
+		let boundingBox = new THREE.Box3().setFromObject(this.picture)
+		let width = boundingBox.max.x - boundingBox.min.x
+		let height = boundingBox.max.y - boundingBox.min.y
+
+		if (this.alignment.includes('left')) {
+			newPosition.x -= width / 2
+		} else if (this.alignment.includes('right')) {
+			newPosition.x += width / 2
+		}
+
+		if (this.alignment.includes('top')) {
+			newPosition.y += height / 2
+		} else if (this.alignment.includes('bottom')) {
+			newPosition.y -= height / 2
+		}
+
+		this.picture.position.set(newPosition.x, newPosition.y, newPosition.z)
+
+		this.picture.scale.set(
+			-newPosition.z * this.scale * 0.06,
+			-newPosition.z * this.scale * 0.06,
+			-newPosition.z * this.scale * 0.06
+		)
 	}
 }
 
-function addPicture({ size, scale, position, rotation, src, loop }) {
-	if (!rotation) rotation = new THREE.Vector3()
-	if (!rotation.x) rotation.x = 0
-	if (!rotation.y) rotation.y = 0
-	if (!rotation.z) rotation.z = 0
-
-	rotation.x = degToRad(rotation.x)
-	rotation.y = degToRad(rotation.y)
-	rotation.z = degToRad(rotation.z)
-
-	pictures.push(new Picture(pictures.length, size, scale, position, rotation, src, loop))
-}
-
 // pictures
-addPicture({
-	size: { x: 1843, y: 2305 },
-	scale: 0.0055,
-	position: {
-		desktop: { x: 7, y: 8, z: -20 },
-		mobile: { x: 7, y: 8, z: -20 }
-	},
-	rotation: { y: -20 },
-	src: './Images/me.png'
-})
+// addPicture({
+// 	size: { x: 1843, y: 2305 },
+// 	scale: 0.0055,
+// 	position: {
+// 		desktop: { x: 7, y: 8, z: -20 },
+// 		mobile: { x: 7, y: 8, z: -20 }
+// 	},
+// 	rotation: { y: -20 },
+// 	src: './Images/me.png'
+// })
 
 // Software Development
-addPicture({
+new Picture({
 	size: { x: 500, y: 500 },
 	scale: 0.007,
-	position: {
-		desktop: { x: -13, y: -28, z: -20 },
-		mobile: { x: -13, y: -28, z: -20 }
-	},
-	rotation: { y: 30 },
+	linkedTo: 'software-dev',
+	alignment: 'right',
+	depth: 10,
+	offset: { x: 25, y: 0 },
+	rotation: { y: 35 },
 	src: './Images/Icons/javascript.png'
 })
-addPicture({
+new Picture({
 	size: { x: 500, y: 500 },
 	scale: 0.007,
-	position: {
-		desktop: { x: 0, y: -39, z: -20 },
-		mobile: { x: 0, y: -39, z: -20 }
-	},
+	linkedTo: 'software-dev',
+	alignment: 'bottom right',
+	depth: 10,
+	offset: { x: 50, y: 70 },
 	rotation: { y: -30, z: -10 },
 	src: './Images/Icons/python.png'
 })
-addPicture({
-	size: { x: 2135, y: 2083 },
-	scale: 0.0015,
-	position: {
-		desktop: { x: -27, y: -49.5, z: -20 },
-		mobile: { x: -27, y: -49.5, z: -20 }
-	},
-	rotation: { z: -30 },
-	src: './Images/Icons/GitHub.png'
-})
+// addPicture({
+// 	size: { x: 2135, y: 2083 },
+// 	scale: 0.0015,
+// 	position: {
+// 		desktop: { x: -27, y: -49.5, z: -20 },
+// 		mobile: { x: -27, y: -49.5, z: -20 }
+// 	},
+// 	rotation: { z: -30 },
+// 	src: './Images/Icons/GitHub.png'
+// })
 
 // Information Technology
-addPicture({
-	size: { x: 588, y: 588 },
-	scale: 0.005,
-	position: {
-		desktop: { x: 6, y: -60, z: -20 },
-		mobile: { x: 6, y: -60, z: -20 }
-	},
-	rotation: { y: 20, z: 10 },
-	src: './Images/Icons/windows.png'
-})
-addPicture({
-	size: { x: 512, y: 512 },
-	scale: 0.005,
-	position: {
-		desktop: { x: 9, y: -71, z: -20 },
-		mobile: { x: 9, y: -71, z: -20 }
-	},
-	rotation: { z: -40, y: -10 },
-	src: './Images/Icons/linux.png'
-})
+// addPicture({
+// 	size: { x: 588, y: 588 },
+// 	scale: 0.005,
+// 	position: {
+// 		desktop: { x: 6, y: -60, z: -20 },
+// 		mobile: { x: 6, y: -60, z: -20 }
+// 	},
+// 	rotation: { y: 20, z: 10 },
+// 	src: './Images/Icons/windows.png'
+// })
+// addPicture({
+// 	size: { x: 512, y: 512 },
+// 	scale: 0.005,
+// 	position: {
+// 		desktop: { x: 9, y: -71, z: -20 },
+// 		mobile: { x: 9, y: -71, z: -20 }
+// 	},
+// 	rotation: { z: -40, y: -10 },
+// 	src: './Images/Icons/linux.png'
+// })
 
 // Accomplishments
-addPicture({
-	size: { x: 1696, y: 2198 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -7, y: -85, z: -23 },
-		mobile: { x: -7, y: -85, z: -23 }
-	},
-	rotation: { y: 15, z: 10 },
-	src: './Images/Certifications and Awards/National Honor Society.png'
-})
+// addPicture({
+// 	size: { x: 1696, y: 2198 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -7, y: -85, z: -23 },
+// 		mobile: { x: -7, y: -85, z: -23 }
+// 	},
+// 	rotation: { y: 15, z: 10 },
+// 	src: './Images/Certifications and Awards/National Honor Society.png'
+// })
 
 
 // Formal Education / Certifications
-addPicture({
-	size: { x: 2187, y: 1632 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -9, y: -138, z: -20 },
-		mobile: { x: -9, y: -138, z: -20 }
-	},
-	rotation: { y: 40 },
-	src: './Images/Honor Roll/21-22-Q2.png'
-})
-addPicture({
-	size: { x: 2187, y: 1622 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -4.4, y: -138, z: -20 },
-		mobile: { x: -4.4, y: -138, z: -20 }
-	},
-	rotation: { y: 0 },
-	src: './Images/Honor Roll/21-22-Q3.png'
-})
-addPicture({
-	size: { x: 2187, y: 1611 },
-	scale: 0.006,
-	position: {
-		desktop: { x: 1, y: -138, z: -20 },
-		mobile: { x: 1, y: -138, z: -20 }
-	},
-	rotation: { y: -40 },
-	src: './Images/Honor Roll/21-22-Q4.png'
-})
+// addPicture({
+// 	size: { x: 2187, y: 1632 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -9, y: -138, z: -20 },
+// 		mobile: { x: -9, y: -138, z: -20 }
+// 	},
+// 	rotation: { y: 40 },
+// 	src: './Images/Honor Roll/21-22-Q2.png'
+// })
+// addPicture({
+// 	size: { x: 2187, y: 1622 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -4.4, y: -138, z: -20 },
+// 		mobile: { x: -4.4, y: -138, z: -20 }
+// 	},
+// 	rotation: { y: 0 },
+// 	src: './Images/Honor Roll/21-22-Q3.png'
+// })
+// addPicture({
+// 	size: { x: 2187, y: 1611 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: 1, y: -138, z: -20 },
+// 		mobile: { x: 1, y: -138, z: -20 }
+// 	},
+// 	rotation: { y: -40 },
+// 	src: './Images/Honor Roll/21-22-Q4.png'
+// })
 
-addPicture({
-	size: { x: 2198, y: 1696 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -9, y: -149, z: -20 },
-		mobile: { x: -9, y: -149, z: -20 }
-	},
-	rotation: { y: 40 },
-	src: './Images/Honor Roll/22-23-Q1.png'
-})
-addPicture({
-	size: { x: 2198, y: 1696 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -4, y: -149, z: -20 },
-		mobile: { x: -4, y: -149, z: -20 }
-	},
-	rotation: { y: 0 },
-	src: './Images/Honor Roll/22-23-Q2.png'
-})
-addPicture({
-	size: { x: 2198, y: 1696 },
-	scale: 0.006,
-	position: {
-		desktop: { x: 1, y: -149, z: -20 },
-		mobile: { x: 1, y: -149, z: -20 }
-	},
-	rotation: { y: -40 },
-	src: './Images/Honor Roll/22-23-Q3.png'
-})
+// addPicture({
+// 	size: { x: 2198, y: 1696 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -9, y: -149, z: -20 },
+// 		mobile: { x: -9, y: -149, z: -20 }
+// 	},
+// 	rotation: { y: 40 },
+// 	src: './Images/Honor Roll/22-23-Q1.png'
+// })
+// addPicture({
+// 	size: { x: 2198, y: 1696 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -4, y: -149, z: -20 },
+// 		mobile: { x: -4, y: -149, z: -20 }
+// 	},
+// 	rotation: { y: 0 },
+// 	src: './Images/Honor Roll/22-23-Q2.png'
+// })
+// addPicture({
+// 	size: { x: 2198, y: 1696 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: 1, y: -149, z: -20 },
+// 		mobile: { x: 1, y: -149, z: -20 }
+// 	},
+// 	rotation: { y: -40 },
+// 	src: './Images/Honor Roll/22-23-Q3.png'
+// })
 
 
-addPicture({
-	size: { x: 2198, y: 1696 },
-	scale: 0.006,
-	position: {
-		desktop: { x: 1, y: -161, z: -20 },
-		mobile: { x: 1, y: -161, z: -20 }
-	},
-	src: './Images/Certifications and Awards/Outstanding Chemistry.png'
-})
-addPicture({
-	size: { x: 2208, y: 1696 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -6, y: -169, z: -20 },
-		mobile: { x: -6, y: -169, z: -20 }
-	},
-	src: './Images/Certifications and Awards/Civic Knowledge.png'
-})
-addPicture({
-	size: { x: 2200, y: 1700 },
-	scale: 0.006,
-	position: {
-		desktop: { x: 2, y: -178, z: -20 },
-		mobile: { x: 2, y: -178, z: -20 }
-	},
-	src: './Images/Certifications and Awards/OSHA Certifacate.png'
-})
-addPicture({
-	size: { x: 2200, y: 1700 },
-	scale: 0.006,
-	position: {
-		desktop: { x: -5, y: -186, z: -20 },
-		mobile: { x: -5, y: -186, z: -20 }
-	},
-	src: './Images/Certifications and Awards/ITF Certifacate.png'
-})
+// addPicture({
+// 	size: { x: 2198, y: 1696 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: 1, y: -161, z: -20 },
+// 		mobile: { x: 1, y: -161, z: -20 }
+// 	},
+// 	src: './Images/Certifications and Awards/Outstanding Chemistry.png'
+// })
+// addPicture({
+// 	size: { x: 2208, y: 1696 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -6, y: -169, z: -20 },
+// 		mobile: { x: -6, y: -169, z: -20 }
+// 	},
+// 	src: './Images/Certifications and Awards/Civic Knowledge.png'
+// })
+// addPicture({
+// 	size: { x: 2200, y: 1700 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: 2, y: -178, z: -20 },
+// 		mobile: { x: 2, y: -178, z: -20 }
+// 	},
+// 	src: './Images/Certifications and Awards/OSHA Certifacate.png'
+// })
+// addPicture({
+// 	size: { x: 2200, y: 1700 },
+// 	scale: 0.006,
+// 	position: {
+// 		desktop: { x: -5, y: -186, z: -20 },
+// 		mobile: { x: -5, y: -186, z: -20 }
+// 	},
+// 	src: './Images/Certifications and Awards/ITF Certifacate.png'
+// })
 
 function isVisible(element) {
 	const rect = element.getBoundingClientRect()
@@ -342,36 +396,42 @@ function isVisible(element) {
 
 let fazLoopTimeout = null
 function scroll() {
-	const scrollAmount = document.body.getBoundingClientRect().top
-	const cameraPosition = scrollAmount * 0.03
-	const screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight)
 
-	pointLight.position.y = cameraPosition + 10
-	camera.position.y = cameraPosition
 
-	const fazTerminalRect = document.getElementById('Faz-Terminal-Box').getBoundingClientRect()
-	const fazTerminalImage = document.getElementById('Faz-Terminal-Image')
-	const fazTerminalVideo = document.getElementById('Faz-Terminal-Video')
-	if (
-		fazTerminalRect.bottom < fazTerminalRect.height * 0.3 ||
-		fazTerminalRect.top >= screenHeight - fazTerminalRect.height * 0.3
-	) {
-		fazTerminalImage.style.display = 'block'
-		fazTerminalVideo.style.display = 'none'
-		fazTerminalVideo.pause()
-		fazTerminalVideo.currentTime = 0
-		fazTerminalVideo.loop = false
-		fazTerminalVideo.src = './Images/Projects/Faz-Terminal-Load.mp4'
-	} else if (fazTerminalImage.style.display != 'none') {
-		fazTerminalImage.style.display = 'none'
-		fazTerminalVideo.style.display = 'block'
-		fazTerminalVideo.play()
-		fazTerminalVideo.onended = () => {
-			fazTerminalVideo.src = './Images/Projects/Faz-Terminal-Loop.mp4'
-			fazTerminalVideo.play()
-			fazTerminalVideo.loop = true
-			fazTerminalVideo.onended = ''
-		}
+	// const scrollAmount = document.body.getBoundingClientRect().top
+	// const cameraPosition = scrollAmount * 0.03
+	// const screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight)
+
+	// pointLight.position.y = cameraPosition + 10
+	// camera.position.y = cameraPosition
+
+	// const fazTerminalRect = document.getElementById('Faz-Terminal-Box').getBoundingClientRect()
+	// const fazTerminalImage = document.getElementById('Faz-Terminal-Image')
+	// const fazTerminalVideo = document.getElementById('Faz-Terminal-Video')
+	// if (
+	// 	fazTerminalRect.bottom < fazTerminalRect.height * 0.3 ||
+	// 	fazTerminalRect.top >= screenHeight - fazTerminalRect.height * 0.3
+	// ) {
+	// 	fazTerminalImage.style.display = 'block'
+	// 	fazTerminalVideo.style.display = 'none'
+	// 	fazTerminalVideo.pause()
+	// 	fazTerminalVideo.currentTime = 0
+	// 	fazTerminalVideo.loop = false
+	// 	fazTerminalVideo.src = './Images/Projects/Faz-Terminal-Load.mp4'
+	// } else if (fazTerminalImage.style.display != 'none') {
+	// 	fazTerminalImage.style.display = 'none'
+	// 	fazTerminalVideo.style.display = 'block'
+	// 	fazTerminalVideo.play()
+	// 	fazTerminalVideo.onended = () => {
+	// 		fazTerminalVideo.src = './Images/Projects/Faz-Terminal-Loop.mp4'
+	// 		fazTerminalVideo.play()
+	// 		fazTerminalVideo.loop = true
+	// 		fazTerminalVideo.onended = ''
+	// 	}
+	// }
+
+	for (const picture of pictures) {
+		picture.setPosition()
 	}
 }
 document.body.onscroll = scroll
@@ -386,6 +446,7 @@ function resizeWindow() {
 
 	for (const picture of pictures) {
 		picture.resize(window.innerWidth / window.innerHeight)
+		picture.setPosition()
 	}
 }
 resizeWindow()
